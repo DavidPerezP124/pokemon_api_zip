9cc5a96fe1621432b8fb564f71791712ddc47506
David A Perez P <davidpp124@gmail.com>
2020-09-16 14:13:35 -0500

    Added google fonts package

--- lib/components/styles/text_styles.dart
+++ lib/components/styles/text_styles.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PokeStyles {
-  PokeStyles({this.context}) : assert(context == null);
+  PokeStyles({this.context}) : assert(context != null);
   final BuildContext context;
   TextStyle get whiteText =>
       Theme.of(context).textTheme.button.copyWith(color: Colors.white);
