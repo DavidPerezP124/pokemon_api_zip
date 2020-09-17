b76b2c80e68bbdc763317a3922d1b5f12d357b21
David A Perez P <davidpp124@gmail.com>
2020-09-17 09:29:11 -0500

    Complete Home, added Jira issue

--- lib/components/home_component.dart
+++ lib/components/home_component.dart
@@ -9,11 +9,14 @@ class HomeComponent extends StatelessWidget {
     Key key,
     @required this.randomList,
     @required this.pokemon,
+    @required this.setLimit,
+    @required this.currentOffset,
   }) : super(key: key);
 
   final List<Pokemon> randomList;
   final List<Pokemon> pokemon;
-
+  final ValueSetter<int> setLimit;
+  final int currentOffset;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -22,32 +25,91 @@ class HomeComponent extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Image.asset("assets/images/pokedeex.png"),
-            Container(
-              height: 200,
-              child: Center(
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: randomList.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: InkWell(
-                          onTap: () => _handleTap(context, randomList[index]),
-                          child: Text(randomList[index].name),
-                        ),
-                      );
-                    }),
+            Center(
+                child: Container(
+                    constraints: BoxConstraints(
+                      maxHeight: 300,
+                      minHeight: 250,
+                    ),
+                    child: Image.asset("assets/images/pokedeex.png"))),
+            Center(
+                child: _titleText(context, title: "Random Picks From Below")),
+            Center(
+              child: Container(
+                height: 100,
+                width: 400,
+                child: Center(
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: randomList.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: InkWell(
+                            onTap: () => _handleTap(context, randomList[index]),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Center(child: Text(randomList[index].name)),
+                                  Center(
+                                      child: Text(randomList[index]
+                                          .url
+                                          .substring(0,
+                                              randomList[index].url.length - 1)
+                                          .split('/')
+                                          .last)),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                ),
               ),
             ),
+            Center(
+                child: ConstrainedBox(
+              constraints: _kWidthBoxConstraints,
+              child: Row(
+                children: [
+                  _titleText(context,
+                      title:
+                          "Pokemon List: ${currentOffset + 1} - ${currentOffset + 100}"),
+                  Spacer(),
+                  IconButton(
+                      icon: Icon(Icons.arrow_back_ios_rounded),
+                      onPressed: () => currentOffset >= 200
+                          ? setLimit(currentOffset - 100)
+                          : () {}),
+                  IconButton(
+                      icon: Icon(Icons.arrow_forward_ios_rounded),
+                      onPressed: () => currentOffset < 1000
+                          ? setLimit(currentOffset + 100)
+                          : () {}),
+                ],
+              ),
+            )),
             Expanded(
               child: Center(
                 child: ListView.builder(
                     itemCount: pokemon.length,
                     itemBuilder: (context, index) {
-                      return Card(
-                        child: ListTile(
-                          leading: Text(pokemon[index].name),
-                          onTap: () => _handleTap(context, pokemon[index]),
+                      return Center(
+                        child: ConstrainedBox(
+                          constraints: _kWidthBoxConstraints,
+                          child: Card(
+                            child: ListTile(
+                              trailing: Text(pokemon[index]
+                                  .url
+                                  .substring(0, pokemon[index].url.length - 1)
+                                  .split('/')
+                                  .last),
+                              leading: Text(pokemon[index].name),
+                              onTap: () => _handleTap(context, pokemon[index]),
+                            ),
+                          ),
                         ),
                       );
                     }),
@@ -67,5 +129,12 @@ class HomeComponent extends StatelessWidget {
               network: network,
               pokemon: int.tryParse(urlList[urlList.length - 2])));
     }));
+  }
+
+  BoxConstraints get _kWidthBoxConstraints => BoxConstraints(
+        maxWidth: 600,
+      );
+  Text _titleText(BuildContext context, {String title}) {
+    return Text(title, style: Theme.of(context).textTheme.headline4);
   }
 }
--- lib/protocols/home_delegate.dart
+++ lib/protocols/home_delegate.dart
@@ -4,5 +4,6 @@ import 'package:pokemon_api_zip/protocols/home_protocol.dart';
 abstract class HomeDelegate {
   HomeProtocol homeDelegate;
   setHomeProtocol(HomeProtocol delegate);
+  getLimited(int offset, {int limit = 100});
   updateHomeDelegate(List<Pokemon> pokemonList);
 }
